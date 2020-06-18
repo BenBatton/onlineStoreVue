66bf6cc41ab198f9e2353481050fde7e38d9c80e
Dmitriy Kozinov <benbatton83@gmail.com>
2020-06-18 20:54:48 +0300

    1

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,23 @@
+<template>
+  <form>
+    <input type="text" />
+    <button type="submit">Create</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style  scoped>
+form {
+  display: flex;
+}
+input {
+  width: 400px;
+}
+</style>>
+
+
+
+</style>
